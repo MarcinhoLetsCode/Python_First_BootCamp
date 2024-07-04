8e76bfd688ae3c918d316edb101e8ff3e2da1899
MarcinhoLetsCode <darlan_lifecat@yahoo.com.br>
2024-07-04 15:56:56 -0300

    Funcionamento Basico Sistema Bancario Versao 1.0

--- challenge_bank.py
+++ challenge_bank.py
@@ -7,9 +7,10 @@ menu = """
 
 ==> """
 
-saldo = 0
+saldo = 1500
+saque_dia = 0
 limite_operacao = 500
-limite_diario = 500
+limite_diario = 1500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
@@ -21,21 +22,48 @@ while True:
     if(opcao == "1"):
         print("Depósito")
         valor = int(input("Insira o Valor Do Depósito: "))
+        
         if(int(valor) > 0):
             saldo += valor
+            extrato += ("+" + str(valor) + "\n" )
         else:
+            print("")
             print("Valor Inserido Invalido!")
 
         print("")
-        extrato += ("+" + str(valor) + "\n" )
         print("Extrato: \n" + extrato)
         print(f"Seu Saldo É De: R${saldo:.2f}")
         
     elif(opcao == "2"):
         print("Saque")
+        valor = int(input("Insira o Valor Para Saque: "))
+        #print("SALDO DIA "+str(int(valor) + saque_dia))
+
+        if(int(valor) <= saldo and int(valor) > 0 and int(valor) <= 500 and ((int(valor) + saque_dia) <= 1500) and numero_saques < LIMITE_SAQUES):
+            saldo -= valor
+            extrato += ("-" + str(valor) + "\n" )
+            numero_saques += 1
+            saque_dia += valor
+            print("")
+            print("Saques Disponiveis: "+str(LIMITE_SAQUES-numero_saques))
+            print("Valor Sacado Dia: "+str(saque_dia))
+        else:
+            print("")
+            print("Valor Inserido Invalido!")
+            print("Saques Disponiveis: "+str(LIMITE_SAQUES-numero_saques))
+            print("Valor Sacado Dia: "+str(saque_dia))
+
+        print("")
+        print("Extrato: \n" + extrato)
+        print(f"Seu Saldo É De: R${saldo:.2f}")
 
     elif(opcao == "3"):
-        print("Extrato")
+        print("Extrato:")
+        print(extrato)
+        if(extrato == ""):
+            print("*Sem Operações No Dia!*")
+            print("")
+        print(f"Seu Saldo É De: R${saldo:.2f}")
          
     elif(opcao == "4"):
         print("Bye")
