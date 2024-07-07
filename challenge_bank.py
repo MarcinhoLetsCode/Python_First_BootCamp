af5e8a02c848d855fb7d24e80d97dd1111c1dae1
MarcinhoLetsCode <darlan_lifecat@yahoo.com.br>
2024-07-07 10:41:43 -0300

    Adicionando Mensagens Individuais

--- challenge_bank.py
+++ challenge_bank.py
@@ -39,7 +39,29 @@ while True:
         valor = float(input("Insira o Valor Para Saque: "))
         #print("SALDO DIA "+str(int(valor) + saque_dia))
 
-        if((valor) <= saldo and float(valor) > 0 and float(valor) <= 500 and ((float(valor) + saque_dia) <= 1500) and numero_saques < LIMITE_SAQUES):
+        if(valor > saldo):
+            print("")
+            print("Operação Cancelada")
+            print("Saldo Insuficiente!")
+        elif(valor < 0):
+            print("")
+            print("Operação Cancelada")
+            print("Valor Informado Inválido!")
+        elif(valor > 500):
+            print("")
+            print("Operação Cancelada")
+            print(f"Tentativa De Saque: {valor} -> Máximo Permitido: R$500.00!")
+        elif(float(valor) + saque_dia > 1500):
+            print("")
+            print("Operação Cancelada")
+            print(f"Valor Sacado Dia: {saque_dia} -> Máximo Permitido: R$1500.00!")
+        elif(numero_saques >= LIMITE_SAQUES):
+            print("")
+            print("Operação Cancelada")
+            print(f"Saques Realizado no Dia: {numero_saques} -> Máximo Permitido: {LIMITE_SAQUES}!")
+        else:
+        
+        #if((valor) <= saldo and float(valor) > 0 and float(valor) <= 500 and ((float(valor) + saque_dia) <= 1500) and numero_saques < LIMITE_SAQUES):
             saldo -= valor
             extrato += (f"R$ -{valor:.2f}\n" )
             numero_saques += 1
@@ -47,24 +69,24 @@ while True:
             print("")
             print("Saques Disponiveis: "+str(LIMITE_SAQUES-numero_saques))
             print("Valor Sacado Dia: "+str(saque_dia))
-        else:
-            print("")
-            print("Valor Inserido Invalido!")
-            print("Saques Disponiveis: "+str(LIMITE_SAQUES-numero_saques))
-            print("Valor Sacado Dia: "+str(saque_dia))
-            print("Valor Máximo Por Saque: R$ 500.00")
+        #else:
+            #print("")
+            #print("Valor Inserido Invalido!")
+            #print("Saques Disponiveis: "+str(LIMITE_SAQUES-numero_saques))
+            #print("Valor Sacado Dia: "+str(saque_dia))
+            #print("Valor Máximo Por Saque: R$ 500.00")
 
         print("")
         print("Extrato: \n" + extrato)
         print(f"Seu Saldo É De: R${saldo:.2f}")
 
     elif(opcao == "3"):
-        print("Extrato:")
+        print("--------------------Extrato--------------------")
         print(extrato)
-        if(extrato == ""):
-            print("*Sem Operações No Dia!*")
-            print("")
+        print("*Sem Operações No Dia!*" if not extrato else extrato)
+        print("")
         print(f"Seu Saldo É De: R${saldo:.2f}")
+        print(" ----------------------------------------------")
          
     elif(opcao == "4"):
         print("Bye")
